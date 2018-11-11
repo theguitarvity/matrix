3de7b4cd245f8098c3b582b66f2ba934a9582238
201519060491 <contato_victorlopes@hotmail.com>
2018-11-11 13:16:56 -0300

    leitura memoria realizada

--- src/matrix/DataMemory.java
+++ src/matrix/DataMemory.java
@@ -1,5 +1,44 @@
 package matrix;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class DataMemory {
+	private ArrayList<Long> memory = new ArrayList<Long>();
+	private int size;
+	
+
+	
+	public ArrayList<Long> data(){
+		return this.memory;
+	}
+	public DataMemory() {
+		// TODO Auto-generated constructor stub
+		//this.memory = new ArrayList<Long>();
+		this.size = memory.size();
+		
+	}
+	public DataMemory(String filename)throws IOException {
+		this();
+		FileInputStream input = new FileInputStream(filename);
+		byte[] bytes = new byte[4];
+		while(input.read(bytes)!=-1) {
+			long word = getUnsigned(bytes[0]);
+			word+=getUnsigned(bytes[1])<<8;
+			word+=getUnsigned(bytes[2])<<16;
+			word+=getUnsigned(bytes[3])<<24;
+			memory.add(word);
+			//push(word);
+		}
+		
+	}
+	private long getUnsigned(byte b) {
+		// TODO Auto-generated method stub
+		if(b>=0)
+			return b;
+		return 256+b;
+		
+	}
 
 }
--- src/matrix/Main.java
+++ src/matrix/Main.java
@@ -5,11 +5,17 @@ import java.io.IOException;
 public class Main {
 	public Main(String[] args) throws IOException{
 		InstructionMemory im  = new InstructionMemory(args[0]);
+		DataMemory dm = new DataMemory(args[1]);
 		Decode dec = new Decode(im.data());
 		dec.decode();
 		for(int i = 0; i < dec.regs().length; i++)
 			System.out.printf("R%d:%d\n",i,dec.regs()[i]);
-		
+	
+		System.out.println("************MEM************");
+		for(Long i : dm.data())
+			System.out.printf("0x%08x\n",i);
+			
+			
 		
 	}
 	private void run() {
